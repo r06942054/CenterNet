c7733b572ae7bf95c4dbee29e89827b99c8228b2
renjie <r06942054@ntu.edu.tw>
2020-05-19 11:29:34 +0800

    [OmniEyes] Update Script.sh

--- script.sh
+++ script.sh
@@ -5,11 +5,11 @@
 
 # copy data from /home/omnieyes/Label  to /home/omnieyes/renjie/GitHub/CenterNet/data
 
-cd data
-mkdir omnieyes
-cd omnieyes
-mkdir training_videos
-mkdir testing_videos
+# cd data
+# mkdir omnieyes
+# cd omnieyes
+# mkdir training_videos
+# mkdir testing_videos
 
 # cp -r /home/omnieyes/Label/data_analysis_20200217/training_videos/* training_videos/
 # cp -r /home/omnieyes/Label/data_analysis_20200217/testing_videos/* testing_videos/
@@ -39,48 +39,96 @@ mkdir testing_videos
 
 # run
 unset PYTHONPATH
-
 conda activate CenterNet
-
 cd /home/omnieyes/renjie/GitHub/CenterNet/src/
 
-# res_18
+:'
+# res_18 with raw data
 python main.py ctdet --exp_id omnieyes_res_18 --batch_size 32 --master_batch 1 --lr 1.25e-4  --gpus 0 --arch res_18 --head_conv 64 --num_epochs 200 --input_h 480 --input_w 800 --flip 0 --no_color_aug
 python test.py --exp_id omnieyes_res_18 --not_prefetch_test ctdet --load_model /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/model_best.pth --arch res_18 --head_conv 64 --input_h 480 --input_w 800
+
+export PYTHONPATH=$PYTHONPATH:/home/omnieyes/renjie/TF_models/research/
+python result_to_tfrecord.py \
+-gt /home/omnieyes/renjie/GitHub/CenterNet/data/omnieyes/annotations/omnieyes_test.json \
+-rt /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/results.json \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/results_test.record
+
+cd /home/omnieyes/renjieOmniEyes_forOD/OmniEyes
+python data_post_processing/inspect_record.py \
+/home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/ \
+-s 0.5 \
+-i 0.5 \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/
+
 
 # dla_34
 # python main.py ctdet --exp_id omnieyes_dla --batch_size 16 --master_batch 1 --lr 1.25e-4  --gpus 0 --num_epochs 200
 # python test.py --exp_id omnieyes_dla --not_prefetch_test ctdet --load_model /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_dla/model_best.pth
 
-# mobilenetv3
+
+unset PYTHONPATH
+conda activate CenterNet
+cd /home/omnieyes/renjie/GitHub/CenterNet/src/
+
+# mobilenetv3 with raw data
 python main.py ctdet --exp_id omnieyes_mobilenetv3 --batch_size 8 --master_batch 1 --lr 1.25e-4  --gpus 0 --arch mobilenetv3 --head_conv 64 --num_epochs 200 --input_h 480 --input_w 800 --flip 0 --no_color_aug
 python test.py --exp_id omnieyes_mobilenetv3 --not_prefetch_test ctdet --load_model /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/model_best.pth --arch mobilenetv3 --head_conv 64 --input_h 480 --input_w 800 --flip 0 --no_color_aug
+export PYTHONPATH=$PYTHONPATH:/home/omnieyes/renjie/TF_models/research/
+python result_to_tfrecord.py \
+-gt /home/omnieyes/renjie/GitHub/CenterNet/data/omnieyes/annotations/omnieyes_test.json \
+-rt /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/results.json \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/results_test.record
 
-# mobilenetv3_2
-python main.py ctdet --exp_id omnieyes_mobilenetv3_2 --batch_size 16 --master_batch 1 --lr 1.25e-4  --gpus 0 --arch mobilenetv3 --head_conv 64 --num_epochs 200 --input_h 480 --input_w 800 --flip 0 --no_color_aug
-python test.py --exp_id omnieyes_mobilenetv3_2 --not_prefetch_test ctdet --load_model /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3_2/model_best.pth --arch mobilenetv3 --head_conv 64 --input_h 480 --input_w 800 --flip 0 --no_color_aug
+cd /home/omnieyes/renjie/OmniEyes_forOD/OmniEyes
+python data_post_processing/inspect_record.py \
+/home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/ \
+-s 0.5 \
+-i 0.5 \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/
+'
 
-# run /home/omnieyes/renjie/GitHub/CenterNet/src/result_to_tfrecord.ipynb
+unset PYTHONPATH
+conda activate CenterNet
+cd /home/omnieyes/renjie/GitHub/CenterNet/src/
 
-# export PYTHONPATH=$PYTHONPATH:/home/omnieyes/renjie/TF_models/research:/home/omnieyes/renjie/TF_models/research/slim
+# res_18 with raw data + Gaussian
+python main.py ctdet --exp_id omnieyes_res_18_gau --batch_size 16 --master_batch 1 --lr 1.25e-4  --gpus 0 --arch res_18 --head_conv 64 --num_epochs 200 --input_h 480 --input_w 800 --flip 0 --no_color_aug
+python test.py --exp_id omnieyes_res_18_gau --not_prefetch_test ctdet --load_model /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18_gau/model_best.pth --arch res_18 --head_conv 64 --input_h 480 --input_w 800
 
-# cd /home/omnieyes/renjie/OmniEyes_forOD/OmniEyes
-# git checkout master
+export PYTHONPATH=$PYTHONPATH:/home/omnieyes/renjie/TF_models/research/
+python result_to_tfrecord.py \
+-gt /home/omnieyes/renjie/GitHub/CenterNet/data/omnieyes/annotations/omnieyes_test.json \
+-rt /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18_gau/results.json \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18_gau/results_test.record
 
-# python data_post_processing/inspect_record.py /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/ \
-# -s 0.5 \
-# -i 0.5 \
-# -o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18/
+cd /home/omnieyes/renjieOmniEyes_forOD/OmniEyes
+python data_post_processing/inspect_record.py \
+/home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18_gau/ \
+-s 0.5 \
+-i 0.5 \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_res_18_gau/
 
-# python data_post_processing/inspect_record.py /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_dla/ \
-# -s 0.5 \
-# -i 0.5 \
-# -o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_dla/
 
-# python data_post_processing/inspect_record.py /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/ \
-# -s 0.5 \
-# -i 0.5 \
-# -o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3/
+unset PYTHONPATH
+conda activate CenterNet
+cd /home/omnieyes/renjie/GitHub/CenterNet/src/
+
+# mobilenetv3 with raw data + Gaussian
+python main.py ctdet --exp_id omnieyes_mobilenetv3_gau --batch_size 8 --master_batch 1 --lr 1.25e-4  --gpus 0 --arch mobilenetv3 --head_conv 64 --num_epochs 200 --input_h 480 --input_w 800 --flip 0 --no_color_aug
+python test.py --exp_id omnieyes_mobilenetv3_gau --not_prefetch_test ctdet --load_model /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3_gau/model_best.pth --arch mobilenetv3 --head_conv 64 --input_h 480 --input_w 800 --flip 0 --no_color_aug
+export PYTHONPATH=$PYTHONPATH:/home/omnieyes/renjie/TF_models/research/
+python result_to_tfrecord.py \
+-gt /home/omnieyes/renjie/GitHub/CenterNet/data/omnieyes/annotations/omnieyes_test.json \
+-rt /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3_gau/results.json \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3_gau/results_test.record
+
+cd /home/omnieyes/renjie/OmniEyes_forOD/OmniEyes
+python data_post_processing/inspect_record.py \
+/home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3_gau/ \
+-s 0.5 \
+-i 0.5 \
+-o /home/omnieyes/renjie/GitHub/CenterNet/exp/ctdet/omnieyes_mobilenetv3_gau/
+
 
 
 # to onnx (輸出須改為list才能轉成onnx，https://github.com/xingyizhou/CenterNet/issues/77#issuecomment-503443329，但改了之後無法train dla，能train res18)
